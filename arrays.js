9e890de9e29ac42670c1870dbc71d6271b962350
Ivan Pratt <ivandoublejr@gmail.com>
2022-02-11 10:09:24 -0700

    starting out

--- arrays.js
+++ arrays.js
@@ -3,12 +3,12 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+const colorCopy = faveColors.slice();
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
+colorCopy.push('blue')
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +18,8 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+const middleNums = numbers.slice(1, -1)
+console.log(middleNums)
 
 
 //////////////////PROBLEM 4////////////////////
