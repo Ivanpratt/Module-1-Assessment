424da0836b879159658e516589e8f3de2196f4a5
Ivan Pratt <ivandoublejr@gmail.com>
2022-02-11 10:45:07 -0700

    halfway 2

--- sldc.js
+++ sldc.js
@@ -21,8 +21,8 @@ Maintain
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysis = ""
-const design = ""
-const implementation = ""
-const testing = ""
-const maintain = ""
+const analysis = "Information gathering stage. The developer will determine more details of things like the environment, hardware, programming langauge that will needed to be implemented in order for the code to work."
+const design = "In this stage the developer designs the code, and what the program will do."
+const implementation = "In this stage, the developer will implement or design the program stated above. This meaning writing out the software as a prototype."
+const testing = "The developer tests the software to ensure it is functioning as intended and can meet the requirements. Changes are done if needed (aka bug fix, feature addition). In the integraton phase, the software is rolled out live."
+const maintain = "This is the ongoing stage where developers monitor, test, and continue to fix or update the software as need. This stage will most likely continue as long as the software is in use."
